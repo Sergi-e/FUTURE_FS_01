fd06cfeceb48a1f16cf74743dd663bbb5d33af42
sergi-e <ishserge16@gmail.com>
2026-03-19 23:02:07 +0200

    feat: Added Navbar styling and install project dependencies.

--- download_books.js
+++ download_books.js
@@ -1,6 +1,10 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const dir = path.join(__dirname, 'public', 'assets', 'books');
 if (!fs.existsSync(dir)) {
